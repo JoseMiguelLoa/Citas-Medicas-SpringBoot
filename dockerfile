95477b8e596a3dae78ad8d044a796ff4e54a06f9
JoseMiguelLoa <josemiguel.lopez.asensio@ces-vegamedia.es>
2024-03-14 14:14:51 +0100

    Revert "Primer commit Programa Funcional excepto los post de citas"
    This reverts commit ef127c330931492e66057b67840aeaf6327922bc.

--- dockerfile
+++
@@ -1,4 +0,0 @@
-FROM openjdk:17-jdk-alpine
-COPY target/Citas-Medicas-0.0.1-SNAPSHOT.jar java-app.jar
-ENTRYPOINT ["java","-jar","java-app.jar"]
-
--- src/main/java/com/example/Citas/Medicas/repositories/ICitaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Citas.Medicas.repositories;
-
-import com.example.Citas.Medicas.models.CitaModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ICitaRepository extends JpaRepository<CitaModel,Long> {
-}
